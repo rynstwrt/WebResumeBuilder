897e5db9e8cd4cbcfcb4f79f9360a4dd6a0a153d
Ryn Stewart <rynstwrt17@gmail.com>
2026-03-25 02:06:06 -0500

    Revert "Wipe"
    This reverts commit b8bd9b3328c02b7855bab416b855c2702c86d631.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Resume Web Builder</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
