a62785b86f846f693efc10d2ecf99f2e485523c2
Ryn Stewart <rynstwrt17@gmail.com>
2026-03-27 01:11:56 -0500

    Update EditorDrawer.svelte

--- src/routes/components/EditorDrawer.svelte
+++ src/routes/components/EditorDrawer.svelte
@@ -9,13 +9,30 @@
 
 
 <script lang="ts">
-    import { Drawer, DrawerHandle } from "flowbite-svelte";
     import { data } from "../data.svelte.ts";
 
     let open = $state(false);
 
-    import { Label, Input, Datepicker, Button } from "flowbite-svelte";
-    import { DrawSquareOutline, EnvelopeSolid, PhoneSolid, UsersSolid, MapPinSolid, BuildingSolid, PlusOutline } from "flowbite-svelte-icons";
+    import {
+        Drawer,
+        DrawerHandle,
+        Label,
+        Input,
+        Datepicker,
+        Button,
+        Accordion,
+        AccordionItem
+    } from "flowbite-svelte";
+
+    import {
+        DrawSquareOutline,
+        EnvelopeSolid,
+        PhoneSolid,
+        UsersSolid,
+        MapPinSolid,
+        BuildingSolid,
+        PlusOutline
+    } from "flowbite-svelte-icons";
 
     let experienceCount = $state(1);
 
@@ -56,12 +73,18 @@
     </DrawerHandle>
 
 
-    <div class="mt-16">
-        <section id="about">
-            <span class="flex">
-                <UsersSolid class="mr-1 h-auto"/>
-                <h3>About</h3>
-            </span>
+    <Accordion class="mt-16">
+        <AccordionItem id="about">
+<!--            <span class="flex">-->
+<!--                <UsersSolid class="mr-1 h-auto"/>-->
+<!--                <h3>About</h3>-->
+<!--            </span>-->
+            {#snippet header()}
+                <div class="flex items-center gap-2">
+                    <UsersSolid/>
+                    <span>About</span>
+                </div>
+            {/snippet}
             <div class="grid gap-2 align-items-end grid-auto-rows grid-cols-1 md:grid-cols-4">
                 <Label for="name">
                     Name
@@ -83,16 +106,19 @@
                     <Input name="location" placeholder="Seattle, WA" bind:value={data.location}/>
                 </Label>
             </div>
-        </section>
+        </AccordionItem>
 
-        <section id="experience">
-            <span class="flex mb-1">
-                <BuildingSolid class="mr-1 h-auto"/>
-                <h3>Experience</h3>
-            </span>
+        <AccordionItem id="experience">
+            {#snippet header()}
+                <div class="flex items-center gap-2">
+                    <BuildingSolid/>
+                    <span>Experience</span>
+                </div>
+            {/snippet}
+
             <div class="grid gap-1 grid-auto-rows grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
                 {#each data.workExperience as job, idx}
-<!--                    <h4 class="text-lg font-light tracking-wide">Experience #{idx + 1}</h4>-->
+                    <!--                    <h4 class="text-lg font-light tracking-wide">Experience #{idx + 1}</h4>-->
                     <div class="border-1 border-gray-300 p-2 ">
                         <h5>{job.company}</h5>
                         <h6>{job.position}</h6>
@@ -105,12 +131,12 @@
                 <div class="mt-4 grid gap-x-2 gap-y-1 align-items-end grid-auto-rows grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
                     <Label for="company" class="md:col-span-2">
                         Company Name
-                        <Input name="company" placeholder="Company Name"}/>
+                        <Input name="company" placeholder="Company Name" }/>
                     </Label>
 
                     <Label for="position" class="md:col-span-2">
                         Position
-                        <Input name="position" placeholder="Job Position"}/>
+                        <Input name="position" placeholder="Job Position" }/>
                     </Label>
 
                     <Label for="job-start" class="md:col-span-2 lg:col-span-1">
@@ -126,8 +152,8 @@
 
                 <Button class="w-fit mt-3 justify-center" onclick={addExperienceClick}>Add Experience</Button>
             </div>
-        </section>
-    </div>
+        </AccordionItem>
+    </Accordion>
 </Drawer>
 
 
