4e00a8ed3d8a948e1fcb828c7ce06be8ffef9dd0
Ryn Stewart <rynstwrt17@gmail.com>
2026-03-28 10:24:14 -0500

    Update data.svelte.ts

--- src/routes/data.svelte.ts
+++ src/routes/data.svelte.ts
@@ -1,19 +1,33 @@
-export const data = $state({
+export const data: {
+    name: string,
+    phone: string,
+    email: string,
+    location: string,
+    workExperience: {
+        company: string | never,
+        position: string | never,
+        start: string | never,
+        end: string | never
+    }[],
+    skills: string[]
+} = $state({
     name: "John Doe",
     phone: "000-000-000",
     email: "john.doe@example.com",
     location: "Seattle, WA",
-    workExperience: [
-        {
-            company: "Oracle",
-            position: "Senior DevOps Engineer",
-            start: "03/03/2020",
-            end: "03/03/2026"
-        }
-    ],
-    skills: ["asdfasdf", "asdffffffffff", "fffffffffffff",
-        "asdfasadf", "asdfffffffafff",
-        "ffffffafffffff", "asdfasdfaaa", "asdffffffffffaaa",
-        "fffffffffffffaaaaa", "asdfasdf"]
+    workExperience: [],
+    skills: []
+    
+    // workExperience: [
+    //     {
+    //         company: "Oracle",
+    //         position: "Senior DevOps Engineer",
+    //         start: "03/03/2020",
+    //         end: "03/03/2026"
+    //     }
+    // ],
+    // skills: ["asdfasdf", "asdffffffffff", "fffffffffffff",
+    //     "asdfasadf", "asdfffffffafff",
+    //     "ffffffafffffff", "asdfasdfaaa", "asdffffffffffaaa",
+    //     "fffffffffffffaaaaa", "asdfasdf"]
 });
-
