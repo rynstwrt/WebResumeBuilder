96f535796da138ef9c6cac79fca9888470fc7913
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-07 23:49:17 -0500

    Start adding editor modal

--- src/App.svelte
+++ src/App.svelte
@@ -16,6 +16,7 @@
         InfoCircleOutline, MapPinAltOutline, PhoneOutline, PhoneSolid, MapPinAltSolid, UserSolid, EnvelopeSolid
     } from "flowbite-svelte-icons";
     import { onDestroy } from "svelte";
+    import EditorModal from "./lib/EditorModal.svelte";
 
     const info = new ResumeData(true);
 
@@ -77,84 +78,11 @@
 </script>
 
 
-<EditorDrawer addToast={addToast} openOnLoad={false} info={info}/>
+<!--<EditorDrawer addToast={addToast} openOnLoad={false} info={info}/>-->
+
+<EditorModal />
 
 
-<!--<Button class="p-2! fixed bottom-5 right-5" onclick={() => (editorOpen = true)}>-->
-<!--    <PenSolid class="h-8 w-8 p-0.5"/>-->
-<!--</Button>-->
-
-<!--<Modal form bind:open={editorOpen} size="md">-->
-<!--    <section>-->
-<!--        <h2 class="text-primary-500 text-2xl tracking-wide">Profile</h2>-->
-<!--        <div class="grid grid-cols-2 auto-rows-auto gap-x-3 gap-y-1">-->
-<!--            <Label for="name" class="mb-2 block">-->
-<!--                Your Name-->
-<!--                <Input name="name" placeholder="John Doe" class="pl-8">-->
-<!--                    {#snippet left()}-->
-<!--                        <UserSolid class="h-5 w-5 text-gray-500 dark:text-gray-400" />-->
-<!--                    {/snippet}-->
-<!--                </Input>-->
-<!--            </Label>-->
-
-<!--            <Label for="email" class="mb-2 block">-->
-<!--                Your Email-->
-<!--                <Input name="email" type="email" placeholder="john.doe@gmail.com" class="pl-8">-->
-<!--                    {#snippet left()}-->
-<!--                        <EnvelopeSolid class="h-5 w-5 text-gray-500 dark:text-gray-400" />-->
-<!--                    {/snippet}-->
-<!--                </Input>-->
-<!--            </Label>-->
-
-<!--            <Label for="telephone" class="mb-2 block">-->
-<!--                Phone Number-->
-<!--                <Input name="telephone" type="tel" placeholder="000-000-000" class="pl-8">-->
-<!--                    {#snippet left()}-->
-<!--                        <PhoneSolid class="h-5 w-5 text-gray-500 dark:text-gray-400" />-->
-<!--                    {/snippet}-->
-<!--                </Input>-->
-<!--            </Label>-->
-
-<!--            <Label for="location" class="mb-2 block">-->
-<!--                Your Location-->
-<!--                <Input name="location" placeholder="Seattle, WA" class="pl-8">-->
-<!--                    {#snippet left()}-->
-<!--                        <MapPinAltSolid class="h-5 w-5 text-gray-500 dark:text-gray-400" />-->
-<!--                    {/snippet}-->
-<!--                </Input>-->
-<!--            </Label>-->
-<!--        </div>-->
-<!--    </section>-->
-<!--</Modal>-->
-
-<!--<Alert border>-->
-<!--    {#snippet icon()}-->
-<!--        <InfoCircleSolid class="h-5 w-5"/>-->
-<!--    {/snippet}-->
-<!--    <span class="font-medium">Successfully added work experience!</span>-->
-<!--</Alert>-->
-
-<!--<Toast>-->
-<!--    {#snippet icon()}-->
-<!--        <InfoCircleOutline class="text-primary-500 bg-primary-100 dark:bg-primary-800 dark:text-primary-200 h-6 w-6"/>-->
-<!--    {/snippet}-->
-<!--    Set yourself free.-->
-<!--</Toast>-->
-
-<ToastContainer position="top-right">
-    {#each toasts as toast (toast.id)}
-        <Toast color={toast.color} dismissable={true}
-               params={{ delay: 100, duration: 500 }} class="w-64"
-               onclose={handleClose(toast.id)} bind:toastStatus={toast.visible}>
-            {#snippet icon()}
-                <CheckCircleSolid class="h-5 w-5"/>
-                <span class="sr-only">Check icon</span>
-            {/snippet}
-            <!--{toast.message}-->
-            <span class="text-gray-300 font-light">{toast.message}</span>
-        </Toast>
-    {/each}
-</ToastContainer>
 
 
 <main class="w-204 h-264 border border-gray-400 bg-white mx-auto p-12 mt-4 mb-12">
---
+++ src/lib/EditorModal.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+    import { Label, Modal, Input, Textarea, Button, P, Select, Tabs, TabItem } from "flowbite-svelte";
+    import { CubesStackedOutline, PaperPlaneSolid, PaperClipOutline, FileSolid, AdjustmentsVerticalSolid, GraduationCapSolid, BuildingSolid, UserCircleSolid } from "flowbite-svelte-icons";
+
+    let open = $state(true);
+    let formError = $state("");
+    let selectedSection = $state("profile");
+
+    let sections: { name: string, value: string }[] = [
+        {name: "Profile", value: "profile"},
+        {name: "Experience", value: "experience"},
+        {name: "Education", value: "education"},
+        {name: "Projects", value: "projects"},
+        {name: "Certifications", value: "certifications"},
+        {name: "Skills", value: "skills"}
+    ];
+
+    function onaction({action, data}: { action: string, data: FormData }) {
+        formError = "";
+
+        console.log(action, data);
+
+        if (action !== "save") {
+            formError = "Action other than save.";
+            return false;
+        }
+    }
+</script>
+
+
+<Modal title="Editor"
+       form
+       bind:open
+       outsideclose={false}
+       {onaction}
+       size="lg"
+       class="flex flex-col items-center w-min">
+    <!--    <Select items={sections} bind:value={selectedSection}/>-->
+
+<!--    {#snippet header()}-->
+<!--        asdf-->
+<!--        <P>asdf</P>-->
+<!--    {/snippet}-->
+
+    <Tabs tabStyle="underline">
+        <TabItem title="Profile" open>
+            {#snippet titleSlot()}
+                <div class="flex items-center gap-2">
+                    <UserCircleSolid size="md" />
+                    Profile
+                </div>
+            {/snippet}
+            <P>Profile. With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.</P>
+        </TabItem>
+
+        <TabItem title="Work" open>
+            {#snippet titleSlot()}
+                <div class="flex items-center gap-2">
+                    <BuildingSolid size="md" />
+                    Work
+                </div>
+            {/snippet}
+            <P>With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.</P>
+        </TabItem>
+
+        <TabItem title="School" open>
+            {#snippet titleSlot()}
+                <div class="flex items-center gap-2">
+                    <GraduationCapSolid size="md" />
+                    School
+                </div>
+            {/snippet}
+            <P>With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.</P>
+        </TabItem>
+
+        <TabItem title="Projects" open>
+            {#snippet titleSlot()}
+                <div class="flex items-center gap-2">
+                    <GraduationCapSolid size="md" />
+                    Projects
+                </div>
+            {/snippet}
+            <P>With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.</P>
+        </TabItem>
+
+        <TabItem title="Certs" open>
+            {#snippet titleSlot()}
+                <div class="flex items-center gap-2">
+                    <GraduationCapSolid size="md" />
+                    Certs
+                </div>
+            {/snippet}
+            <P>With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.</P>
+        </TabItem>
+
+        <TabItem title="Skills" open>
+            {#snippet titleSlot()}
+                <div class="flex items-center gap-2">
+                    <GraduationCapSolid size="md" />
+                    Skills
+                </div>
+            {/snippet}
+            <P>With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                companies around the world are updating their terms of service agreements to comply.</P>
+        </TabItem>
+    </Tabs>
+
+
+    <!--{#snippet footer()}-->
+    <!--        <Button type="submit" value="success">I accept</Button>-->
+    <!--        <Button type="submit" value="decline" color="alternative" class="text-primary-100">Decline</Button>-->
+    <!--    {/snippet}-->
+</Modal>
+
+<Button pill onclick={() => (open = true)}>
+    <AdjustmentsVerticalSolid/>
+</Button>
