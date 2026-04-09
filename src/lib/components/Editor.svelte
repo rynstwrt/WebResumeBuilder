f1a836417272baccc8be9851b0868e25fe3442d4
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-08 19:43:48 -0500

    Add tabs to modal

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -4,7 +4,9 @@
         Label,
         Input,
         Button,
-        Textarea
+        Textarea,
+        Tabs,
+        TabItem
     } from "flowbite-svelte";
 
     import {
@@ -34,113 +36,163 @@
        size="sm">
     <div id="editor-modal">
 
-        <div class="editor-section mt-5">
-            <h3>Profile</h3>
-            <div class="grid grid-cols-2 gap-3">
-                <div>
-                    <Label for="name" class="mb-0.5 text-sm">Name</Label>
-                    <Input name="name"
-                           size="md"
-                           placeholder="John Doe"
-                           required
-                           bind:value={info.name}/>
+        <Tabs tabStyle="underline" class="mt-5">
+            <TabItem title="Profile">
+                {#snippet titleSlot()}
+                    <UserCircleSolid size="md"/>
+                {/snippet}
+                <div class="editor-section">
+                    <h3>Profile</h3>
+                    <div class="grid grid-cols-2 gap-2">
+                        <div>
+                            <Label for="name" class="mb-0.5 text-sm">Name</Label>
+                            <Input name="name"
+                                   size="md"
+                                   placeholder="John Doe"
+                                   required
+                                   bind:value={info.name}/>
+                        </div>
+                        <div>
+                            <Label for="phone" class="mb-0.5 text-sm">Phone Number</Label>
+                            <Input name="phone"
+                                   type="tel"
+                                   placeholder="123-456-7890"
+                                   required
+                                   bind:value={info.phone}/>
+                        </div>
+                        <div>
+                            <Label for="email" class="mb-0.5 text-sm">Email</Label>
+                            <Input name="email"
+                                   placeholder="john.doe@gmail.com"
+                                   required
+                                   bind:value={info.email}/>
+                        </div>
+                        <div>
+                            <Label for="location" class="mb-0.5 text-sm">Location</Label>
+                            <Input name="location"
+                                   placeholder="Seattle, WA"
+                                   required
+                                   bind:value={info.location}/>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <Label for="phone" class="mb-0.5 text-sm">Phone Number</Label>
-                    <Input name="phone"
-                           type="tel"
-                           placeholder="123-456-7890"
-                           required
-                           bind:value={info.phone}/>
+            </TabItem>
+
+
+            <TabItem title="Work">
+                {#snippet titleSlot()}
+                    <BuildingSolid/>
+                {/snippet}
+                <div class="editor-section">
+                    <h3>Work</h3>
+                    <Button size="sm" class="w-fit font-normal">
+                        <CirclePlusOutline size="sm" class="mr-1"/>
+                        Experience
+                    </Button>
                 </div>
-                <div>
-                    <Label for="email" class="mb-0.5 text-sm">Email</Label>
-                    <Input name="email"
-                           placeholder="john.doe@gmail.com"
-                           required
-                           bind:value={info.email}/>
+            </TabItem>
+
+
+            <TabItem title="Education">
+                {#snippet titleSlot()}
+                    <GraduationCapSolid/>
+                {/snippet}
+                <div class="editor-section">
+                    <h3>Education</h3>
+                    <Button size="sm" class="w-fit font-normal">
+                        <CirclePlusOutline size="sm" class="mr-1"/>
+                        Education
+                    </Button>
                 </div>
-                <div>
-                    <Label for="location" class="mb-0.5 text-sm">Location</Label>
-                    <Input name="location"
-                           placeholder="Seattle, WA"
-                           required
-                           bind:value={info.location}/>
+            </TabItem>
+
+
+            <TabItem title="Projects">
+                {#snippet titleSlot()}
+                    <PaperPlaneSolid/>
+                {/snippet}
+                <div class="editor-section">
+                    <h3>Projects</h3>
+                    <Button size="sm" class="w-fit font-normal">
+                        <CirclePlusOutline size="sm" class="mr-1"/>
+                        Project
+                    </Button>
                 </div>
-            </div>
-        </div>
+            </TabItem>
 
 
-        <div class="editor-section">
-            <h3>Work</h3>
-            <Button size="sm" class="w-fit font-normal">
-                <CirclePlusOutline size="sm" class="mr-1"/>
-                Experience
-            </Button>
-        </div>
+            <TabItem title="Certifications" open>
+                {#snippet titleSlot()}
+                    <FileSolid />
+                {/snippet}
+                <div class="editor-section">
+                    <h3>Certifications</h3>
+
+                    <div class="grid grid-cols-2 gap-2">
+                        <div>
+                            <Label for="cert-name-input" class="mb-0.5 text-sm">Name</Label>
+                            <Input name="cert-name-input" placeholder="CCNA"/>
+                        </div>
+                        <div>
+                            <Label for="cert-dates-input" class="mb-0.5 text-sm">Dates</Label>
+                            <Input name="cert-dates-input" placeholder="2025 - Present"/>
+                        </div>
+                        <div class="w-full col-span-full">
+                            <Label for="cert-bullet-input" class="mb-0.5 text-sm">Bulletpoints</Label>
+                            <Textarea rows={6}
+                                      name="cert-bullet-input"
+                                      class="w-full"
+                                      placeholder="Type bulletpoints, one per each line..."/>
+                        </div>
+                        <Button size="sm" class="w-fit font-normal">
+                            <CirclePlusOutline size="sm" class="mr-1"/>
+                            Add
+                        </Button>
+                    </div>
+                </div>
+            </TabItem>
 
 
-        <div class="editor-section">
-            <h3>Skills</h3>
+            <TabItem title="Skills">
+                {#snippet titleSlot()}
+                    <PenSolid/>
+                {/snippet}
+                <div class="editor-section">
+                    <h3>Skills</h3>
 
-            <Textarea rows={6}
-                      class="w-full"
-                      placeholder="Type skills separated by commas..."
-                      bind:value={info.skills}/>
-
-
-            <!--            <div class="flex flex-wrap items-center text-nowrap gap-2">-->
-            <!--                {#each info.skills as skill}-->
-            <!--                    <Button size="xs" class="w-min">{skill}</Button>-->
-            <!--                {/each}-->
-            <!--            </div>-->
-
-            <!--            <Button size="sm" class="w-fit font-normal">-->
-            <!--                <CirclePlusOutline size="sm" class="mr-1"/>-->
-            <!--                Skill-->
-            <!--            </Button>-->
-        </div>
-
-        <div class="editor-section">
-            <h3>School</h3>
-            <Button size="sm" class="w-fit font-normal">
-                <CirclePlusOutline size="sm" class="mr-1"/>
-                School
-            </Button>
-        </div>
+                    <Textarea rows={6}
+                              class="w-full"
+                              placeholder="Type skills separated by commas..."
+                              bind:value={info.skills}/>
 
 
-        <div class="editor-section">
-            <h3>Projects</h3>
-            <Button size="sm" class="w-fit font-normal">
-                <CirclePlusOutline size="sm" class="mr-1"/>
-                Project
-            </Button>
-        </div>
+                    <!--            <div class="flex flex-wrap items-center text-nowrap gap-2">-->
+                    <!--                {#each info.skills as skill}-->
+                    <!--                    <Button size="xs" class="w-min">{skill}</Button>-->
+                    <!--                {/each}-->
+                    <!--            </div>-->
 
-
-        <div class="editor-section">
-            <h3>Certifications</h3>
-            <Button size="sm" class="w-fit font-normal">
-                <CirclePlusOutline size="sm" class="mr-1"/>
-                Certification
-            </Button>
-        </div>
-
+                    <!--            <Button size="sm" class="w-fit font-normal">-->
+                    <!--                <CirclePlusOutline size="sm" class="mr-1"/>-->
+                    <!--                Skill-->
+                    <!--            </Button>-->
+                </div>
+            </TabItem>
+        </Tabs>
 
     </div>
 
     {#snippet footer()}
         <Button type="submit" color="green" class="w-full">
-            <CheckCircleSolid size="md" class="mr-1" />
+            <CheckCircleSolid size="md" class="mr-1"/>
             Confirm
         </Button>
         <Button color="secondary">
-            <UploadSolid size="md" class="mr-1" />
+            <UploadSolid size="md" class="mr-1"/>
             Import
         </Button>
         <Button color="secondary">
-            <DownloadSolid size="md" class="mr-1" />
+            <DownloadSolid size="md" class="mr-1"/>
             Export
         </Button>
     {/snippet}
@@ -150,3 +202,12 @@
 <Button pill onclick={() => (editorOpen = true)} class="w-10 h-10">
     <AdjustmentsVerticalSolid/>
 </Button>
+
+
+<style>
+    @import "tailwindcss";
+
+    .editor-section h3 {
+        @apply text-2xl mb-4 text-white font-light;
+    }
+</style>
