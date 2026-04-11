3f132037a39da6fa12ea7f87a179a992ca14c252
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-10 21:46:18 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -11,10 +11,10 @@
         PhoneOutline,
     } from "flowbite-svelte-icons";
 
-    const devMode = process.env.NODE_ENV === "development";
+    const isDevMode = process.env.NODE_ENV === "development";
 
     import ResumeData from "./lib/ResumeInfo.svelte.ts";
-    const info = new ResumeData(devMode);
+    const info = new ResumeData(isDevMode);
 
     import Editor from "./lib/components/Editor.svelte";
 
@@ -41,11 +41,11 @@
 
 
 <!--<EditorDrawer addToast={addToast} openOnLoad={false} info={info}/>-->
-<Editor openOnLoad={devMode} info={info} {downloadPDF}/>
+<Editor openOnLoad={isDevMode} info={info} {downloadPDF}/>
 
 <main
         class="w-204 min-h-264 bg-white mx-auto p-12 mt-4 mb-12">
-<!--    class="w-204 min-h-264 bg-white mx-auto p-12 mt-4 mb-12">-->
+    <!--    class="w-204 min-h-264 bg-white mx-auto p-12 mt-4 mb-12">-->
     <!--    class="w-204 h-264 border border-gray-400 bg-white mx-auto p-12 mt-4 mb-12">-->
     <!-- ABOUT SECTION  -->
 
@@ -95,126 +95,132 @@
     </section>
 
     <!-- EXPERIENCE SECTION -->
+    {#if info.workExperience?.length}
+        <section id="experience">
+            <h2 class="header">Experience</h2>
 
-    <section id="experience">
-        <h2 class="header">Experience</h2>
+            {#each info.workExperience as entry}
+                <div class="tile">
+                    <div class="flex justify-between">
+                        <div>
+                            <h3 class="subheader">{entry.role}</h3>
+                            <h4 class="subheader-2">{entry.employer}</h4>
+                        </div>
 
-        {#each info.workExperience as entry}
-            <div class="tile">
-                <div class="flex justify-between">
-                    <div>
-                        <h3 class="subheader">{entry.role}</h3>
-                        <h4 class="subheader-2">{entry.employer}</h4>
+                        <div class="dates text-right tracking-tight">
+                            <!--                        <p>{entry.start} - {entry.end || "Present"}</p>-->
+                            <p>{entry.dates}</p>
+                            <p>Dallas, TX</p>
+                        </div>
                     </div>
 
-                    <div class="dates text-right tracking-tight">
-                        <!--                        <p>{entry.start} - {entry.end || "Present"}</p>-->
-                        <p>{entry.dates}</p>
-                        <p>Dallas, TX</p>
-                    </div>
+                    <!--                <div class="flex items-center">-->
+                    <!--                    <h3 class="subheader">{entry.title}</h3>-->
+                    <!--                    <p class="dates ml-2">{entry.start} - {entry.end || "Present"}</p>-->
+                    <!--                </div>-->
+                    <!--                <h4 class="subheader-2">{entry.employer}</h4>-->
+                    <!--                <p class="description">{entry.description}</p>-->
+
+                    {#if entry.bulletpoints?.length}
+                        <ul class="list-disc pl-[1.5ch] mt-0.5">
+                            {#each entry.bulletpoints as bulletpoint}
+                                <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
+                                <li class="text-sm font-light">{bulletpoint}</li>
+                            {/each}
+                        </ul>
+                    {/if}
                 </div>
-
-                <!--                <div class="flex items-center">-->
-                <!--                    <h3 class="subheader">{entry.title}</h3>-->
-                <!--                    <p class="dates ml-2">{entry.start} - {entry.end || "Present"}</p>-->
-                <!--                </div>-->
-                <!--                <h4 class="subheader-2">{entry.employer}</h4>-->
-                <!--                <p class="description">{entry.description}</p>-->
-
-                {#if entry.bulletpoints}
-                    <ul class="list-disc pl-[1.5ch] mt-0.5">
-                        {#each entry.bulletpoints as bulletpoint}
-                            <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
-                            <li class="text-sm font-light">{bulletpoint}</li>
-                        {/each}
-                    </ul>
-                {/if}
-            </div>
-        {/each}
-    </section>
+            {/each}
+        </section>
+    {/if}
 
     <!-- EDUCATION SECTION -->
+    {#if info.education?.length}
+        <section id="education">
+            <h2 class="header">Education</h2>
 
-    <section id="education">
-        <h2 class="header">Education</h2>
+            {#each info.education as entry}
+                <div class="tile leading-5.5 flex justify-between">
+                    <div>
+                        <h3 class="subheader">{entry.school}</h3>
+                        <h4 class="subheader-2">{entry.diploma}</h4>
+                        <p class="dates">{entry.dates}</p>
+                        <!--                    <p class="dates">{entry.start} - {entry.end || "Present"}</p>-->
+                        <!--                <p class="dates">{entry.start} - {entry.end || "Present"}, Richardson, TX</p>-->
+                    </div>
 
-        {#each info.education as entry}
-            <div class="tile leading-5.5 flex justify-between">
-                <div>
-                    <h3 class="subheader">{entry.school}</h3>
-                    <h4 class="subheader-2">{entry.diploma}</h4>
-                    <p class="dates">{entry.dates}</p>
-                    <!--                    <p class="dates">{entry.start} - {entry.end || "Present"}</p>-->
-                    <!--                <p class="dates">{entry.start} - {entry.end || "Present"}, Richardson, TX</p>-->
+                    <!--                <div>-->
+                    <!--                    <h3 class="subheader">{entry.school}</h3>-->
+                    <!--                    <h4 class="subheader-2">{entry.diploma}</h4>-->
+                    <!--                    <p class="dates">Richardson, TX</p>-->
+                    <!--                    &lt;!&ndash;                <p class="dates">{entry.start} - {entry.end || "Present"}, Richardson, TX</p>&ndash;&gt;-->
+                    <!--                </div>-->
+                    <!--                <div>-->
+                    <!--                    <p class="dates text-right">{entry.start} - {entry.end || "Present"}</p>-->
+                    <!--                </div>-->
                 </div>
-
-                <!--                <div>-->
-                <!--                    <h3 class="subheader">{entry.school}</h3>-->
-                <!--                    <h4 class="subheader-2">{entry.diploma}</h4>-->
-                <!--                    <p class="dates">Richardson, TX</p>-->
-                <!--                    &lt;!&ndash;                <p class="dates">{entry.start} - {entry.end || "Present"}, Richardson, TX</p>&ndash;&gt;-->
-                <!--                </div>-->
-                <!--                <div>-->
-                <!--                    <p class="dates text-right">{entry.start} - {entry.end || "Present"}</p>-->
-                <!--                </div>-->
-            </div>
-        {/each}
-    </section>
+            {/each}
+        </section>
+    {/if}
 
     <!-- PROJECTS SECTION -->
+    {#if info.projects}
+        <section id="projects">
+            <h2 class="header">Projects</h2>
 
-    <section id="projects">
-        <h2 class="header">Projects</h2>
-
-        {#each info.projects as entry}
-            <div class="tile">
-                <h3 class="subheader">{entry.title}</h3>
-                <!--                <p class="font-light text-sm">{entry.description}</p>-->
-                {#if entry.bulletpoints}
-                    <ul class="list-disc pl-[1.5ch] mt-0.5">
-                        {#each entry.bulletpoints as bulletpoint}
-                            <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
-                            <li class="text-sm font-light">{bulletpoint}</li>
-                        {/each}
-                    </ul>
-                {/if}
-                {#each entry.links as link}
-                    <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
-                    <a href={link} class="block text-sm font-light">{link}</a>
-                {/each}
-            </div>
-        {/each}
-    </section>
+            {#each info.projects as entry}
+                <div class="tile">
+                    <h3 class="subheader">{entry.title}</h3>
+                    <!--                <p class="font-light text-sm">{entry.description}</p>-->
+                    {#if entry.bulletpoints?.length}
+                        <ul class="list-disc pl-[1.5ch] mt-0.5">
+                            {#each entry.bulletpoints as bulletpoint}
+                                <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
+                                <li class="text-sm font-light">{bulletpoint}</li>
+                            {/each}
+                        </ul>
+                    {/if}
+                    {#each entry.links as link}
+                        <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
+                        <a href={link} class="block text-sm font-light">{link}</a>
+                    {/each}
+                </div>
+            {/each}
+        </section>
+    {/if}
 
     <!-- CERTIFICATIONS SECTION -->
+    {#if info.certifications?.length}
+        <section id="certifications">
+            <h2 class="header">Certifications</h2>
 
-    <section id="certifications">
-        <h2 class="header">Certifications</h2>
+            {#each info.certifications as entry}
+                <div class="tile">
+                    <h3 class="subheader">{entry.title}</h3>
+                    <!--                <p class="font-light text-sm">{entry.description}</p>-->
+                    <!--                <p class="dates">{entry.start} - {entry.end || "Present"}</p>-->
+                    <p class="dates">{entry.dates}</p>
+                    {#if entry.bulletpoints?.length}
+                        <ul class="list-disc pl-[1.5ch] mt-0.5">
+                            {#each entry.bulletpoints as bulletpoint}
+                                <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
+                                <li class="text-sm font-light">{bulletpoint}</li>
+                            {/each}
+                        </ul>
+                    {/if}
+                </div>
+            {/each}
+        </section>
+    {/if}
 
-        {#each info.certifications as entry}
-            <div class="tile">
-                <h3 class="subheader">{entry.title}</h3>
-<!--                <p class="font-light text-sm">{entry.description}</p>-->
-<!--                <p class="dates">{entry.start} - {entry.end || "Present"}</p>-->
-                <p class="dates">{entry.dates}</p>
-                {#if entry.bulletpoints}
-                    <ul class="list-disc pl-[1.5ch] mt-0.5">
-                        {#each entry.bulletpoints as bulletpoint}
-                            <!--                            <li class="text-sm font-light">• {bulletpoint}</li>-->
-                            <li class="text-sm font-light">{bulletpoint}</li>
-                        {/each}
-                    </ul>
-                {/if}
-            </div>
-        {/each}
-    </section>
 
     <!-- SKILLS SECTION -->
-
-    <section id="skills">
-        <h2 class="header">Skills</h2>
-        <p class="text-xs font-light">{info.skills}</p>
-    </section>
+    {#if info.skills?.length}
+        <section id="skills">
+            <h2 class="header">Skills</h2>
+            <p class="text-xs font-light">{info.skills}</p>
+        </section>
+    {/if}
 </main>
 
 <style>
