0bf859a73413a9a6aac6dd033222589a7a4e4418
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-10 21:27:56 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+export default defineConfig({
+    plugins: [tailwindcss(), svelte()],
+    base: "/ResumeWebBuilder/"
+});
