3ddababecdfb98e9611584e1c86023fa7bfc6690
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-12 15:42:36 -0500

    Consolidate sections into App.svelte file

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,27 @@
 
     import html2pdf from "html2pdf.js";
     import html2canvas from "html2canvas-pro";
-    import ProfileSection from "./lib/components/sections/ProfileSection.svelte";
-    import WorkSection from "./lib/components/sections/WorkSection.svelte";
-    import EducationSection from "./lib/components/sections/EducationSection.svelte";
-    import ProjectsSection from "./lib/components/sections/ProjectsSection.svelte";
-    import CertificationsSection from "./lib/components/sections/CertificationsSection.svelte";
-    import SkillsSection from "./lib/components/sections/SkillsSection.svelte";
+
+    import { Li, P } from "flowbite-svelte";
+
+    import {
+        EnvelopeOutline,
+        GlobeOutline,
+        MapPinAltOutline,
+        PhoneOutline
+    } from "flowbite-svelte-icons";
+
+    import {
+        certificationsData,
+        educationData,
+        profileData,
+        projectsData,
+        skillsData,
+        workData
+    } from "./lib/ResumeData.svelte.ts";
+
+    import Section from "./lib/components/Section.svelte";
+
 
     function downloadPDF() {
         const el: HTMLElement = document.querySelector("main")!;
@@ -31,19 +46,136 @@
 </script>
 
 
-
-<Editor />
+<Editor/>
 
 
 <main class="w-204 min-h-264 bg-white mx-auto p-12 mt-4 mb-12">
-    <ProfileSection />
-    <EducationSection />
-    <WorkSection />
-    <ProjectsSection />
-    <CertificationsSection />
-    <SkillsSection />
-</main>
+    <section id="profile" class="text-center flex flex-col items-center">
+        <h1 class="text-3xl font-semibold mb-1">{profileData.name}</h1>
+        <div class="text-xs font-light flex gap-2">
+            {#if profileData.location}
+            <span>
+				<MapPinAltOutline size="sm" class="inline"/>
+                {profileData.location}
+			</span>
+                {#if profileData.phone || profileData.email}
+                    |
+                {/if}
+            {/if}
+            {#if profileData.phone}
+            <span>
+                <PhoneOutline size="sm" class="inline"/>
+                {profileData.phone}
+            </span>
+                {#if profileData.email}
+                    |
+                {/if}
+            {/if}
+            {#if profileData.email}
+            <span>
+                <EnvelopeOutline size="sm" class="inline"/>
+                {profileData.email}
+            </span>
+            {/if}
+        </div>
+        {#if profileData?.links?.length}
+            <div class="flex gap-2 text-xs font-light">
+                {#each profileData.links as link, idx}
+                    <span>
+                        <GlobeOutline size="sm" class="inline"/>
+                        <a href={link}>{link}</a>
+                    </span>
+                    {#if idx !== profileData.links.length - 1}
+                        |
+                    {/if}
+                {/each}
+            </div>
+        {/if}
+    </section>
 
+    {#if workData.length}
+        <Section title="Experience">
+            {#each workData as entry}
+                <div class="entry">
+                    <div class="flex justify-between">
+                        <div>
+                            <h3 class="header">{entry.company}</h3>
+                            <h4 class="subheader">{entry.position}</h4>
+                        </div>
+                        <div>
+                            <P class="detail">{entry.city}</P>
+                            <P class="detail">{entry.dates}</P>
+                        </div>
+                    </div>
+                    {#if entry.bulletpoints?.length}
+                        <ul class="list-disc pl-[1.5ch] mt-0.5">
+                            {#each entry.bulletpoints as bulletpoint}
+                                <Li class="text-sm font-light">{bulletpoint}</Li>
+                            {/each}
+                        </ul>
+                    {/if}
+                </div>
+            {/each}
+        </Section>
+    {/if}
+
+    {#if educationData.length}
+        <Section title="Education">
+            {#each educationData as entry}
+                <div class="entry flex justify-between">
+                    <div class="w-full flex justify-between">
+                        <div>
+                            <h3 class="header">{entry.school}</h3>
+                            <h4 class="subheader">{entry.degree}</h4>
+                        </div>
+                        <div class="text-right tracking-tight">
+                            <P class="detail">{entry.city}</P>
+                            <P class="detail">{entry.date}</P>
+                        </div>
+                    </div>
+                </div>
+            {/each}
+        </Section>
+    {/if}
+
+    {#if projectsData.length}
+        <Section title="Projects">
+            {#each projectsData as entry}
+                <div class="entry">
+                    <h3 class="header">{entry.name}</h3>
+                    <a class="subheader" href={entry.link}>{entry.link}</a>
+                    {#if entry.bulletpoints?.length}
+                        <ul class="list-disc pl-[1.5ch] mt-0.5">
+                            {#each entry.bulletpoints as bulletpoint}
+                                <Li class="text-sm font-light">{bulletpoint}</Li>
+                            {/each}
+                        </ul>
+                    {/if}
+                </div>
+            {/each}
+        </Section>
+    {/if}
+
+    {#if certificationsData.length}
+        <Section title="Certifications">
+            {#each certificationsData as entry}
+                <div class="entry flex justify-between">
+                    <div class="">
+                        <h3 class="header">{entry.name}</h3>
+                        <h4 class="subheader">{entry.from}</h4>
+                    </div>
+
+                    <P class="detail">{entry.date}</P>
+                </div>
+            {/each}
+        </Section>
+    {/if}
+    {#if skillsData.skills.length}
+        <Section title="Skills">
+            <P class="detail text-left!">{skillsData.skills.join(", ")}.</P>
+        </Section>
+    {/if}
+</main>
 
 
 <style>
@@ -55,13 +187,13 @@
     }
 
     /*:global(section) {*/
-        /*&:not(:last-of-type) {*/
-        /*    @apply mb-6;*/
-        /*}*/
+    /*&:not(:last-of-type) {*/
+    /*    @apply mb-6;*/
+    /*}*/
 
-        /*:global(.tile) {*/
-        /*    @apply py-2.5;*/
-        /*}*/
+    /*:global(.tile) {*/
+    /*    @apply py-2.5;*/
+    /*}*/
     /*}*/
 
 
