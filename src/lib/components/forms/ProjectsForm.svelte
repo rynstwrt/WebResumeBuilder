ae4c3f26b99c47ca3bef0ef9816ef37d07eb55dd
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-14 07:01:21 -0500

    Clean and update favicon color

--- src/lib/components/forms/ProjectsForm.svelte
+++ src/lib/components/forms/ProjectsForm.svelte
@@ -1,30 +1,5 @@
 <script lang="ts">
-    let {open = $bindable()} = $props();
-
-    import {
-        TextEditor,
-        ToolbarRowWrapper,
-        AlignmentButtonGroup,
-        CharacterCount,
-        DetailsButtonGroup,
-        Divider,
-        EditableButton,
-        ExportButtonGroup,
-        FormatButtonGroup,
-        HeadingButtonGroup,
-        ImageButtonGroup,
-        InvisibleButtonGroup,
-        LayoutButtonGroup,
-        ListButtonGroup,
-        SourceButtonGroup,
-        TableButtonGroup1,
-        TableButtonGroup2,
-        TaskListButtonGroup,
-        UndoRedoButtonGroup,
-        YoutubeButtonGroup
-    } from "@flowbite-svelte-plugins/texteditor";
-
     import { Button } from "flowbite-svelte";
-
-    import type { Editor } from "@tiptap/core";
 </script>
+
+<p>Projects form</p>
