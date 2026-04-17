a273511fb6af8bae1230930c90d307da18c9abaf
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-17 10:59:10 -0500

    Add education functionality

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,12 @@
         Drawer,
         Input,
         Label,
-        Tags
+        Tags,
+        Textarea,
+        Timeline,
+        TimelineItem,
+        Datepicker,
+        Checkbox
     } from "flowbite-svelte";
 
     import {
@@ -16,8 +21,13 @@
         GraduationCapOutline,
         PaperClipOutline,
         PaperPlaneOutline,
-        UserOutline
+        UserOutline,
+        PlusOutline,
+        CloseOutline,
+        SchoolOutline
     } from "flowbite-svelte-icons";
+
+    import moment from "moment";
 
     import ResumePreview from "./lib/components/ResumePreview.svelte";
 
@@ -45,38 +55,47 @@
     let email: string = $state("");
     let links: string[] = $state([]);
 
-    // let sections = [
-    //     {
-    //         label: "Profile",
-    //         Icon: UserOutline,
-    //         Form: ProfileForm
-    //     },
-    //     {
-    //         label: "Work Experience",
-    //         Icon: BuildingOutline,
-    //         Form: WorkForm
-    //     },
-    //     {
-    //         label: "Education",
-    //         Icon: GraduationCapOutline,
-    //         Form: EducationForm,
-    //     },
-    //     {
-    //         label: "Projects",
-    //         Icon: PaperPlaneOutline,
-    //         Form: ProjectsForm
-    //     },
-    //     {
-    //         label: "Certifications",
-    //         Icon: FileLinesOutline,
-    //         Form: CertificationsForm
-    //     },
-    //     {
-    //         label: "Skills",
-    //         Icon: PaperClipOutline,
-    //         Form: SkillsForm
-    //     }
-    // ]
+    let education = $state([
+        {
+            id: 0,
+            school: "The University of Texas",
+            concentration: "B.S. Electrical Engineering",
+            startDate: new Date("August 1, 2019"),
+            endDate: new Date("Dec 1, 2025"),
+            ongoing: false,
+            summary: "summarytext"
+        },
+        {
+            id: 1,
+            school: "Plano West Senior High School",
+            concentration: "High School Diploma",
+            startDate: new Date("August 1, 2015"),
+            endDate: new Date("June 1, 2019"),
+            ongoing: false,
+            summary: "summarytext"
+        }
+    ]);
+
+    let educationForm = $state({
+        id: 0,
+        school: "",
+        concentration: "",
+        startDate: undefined,
+        endDate: undefined,
+        ongoing: false,
+        summary: ""
+    });
+
+    const DATE_FORMAT = "MMM YYYY";
+    let sortedEducation = $derived([...education].sort((a, b) => {
+        let aMoment = moment(a.endDate);
+        let bMoment = moment(b.endDate);
+
+        if (aMoment.isSame(bMoment))
+            return 0;
+
+        return aMoment.isAfter(bMoment) ? 1 : -1;
+    }));
 </script>
 
 
@@ -96,7 +115,7 @@
         width="half"
         id="drawer">
     <Accordion class="mt-10" flush={true}>
-        <AccordionItem open>
+        <AccordionItem>
             {#snippet header()}
                 <span class="flex items-center">
                     <UserOutline size="md" class="me-1"/>
@@ -122,19 +141,95 @@
                 </div>
                 <div class="col-span-full">
                     <Label for="links" class="text-sm font-normal leading-6">Links</Label>
-                    <Tags id="links" placeholder="Enter links here and press enter" bind:value={links} />
+                    <Tags id="links" placeholder="Enter links here and press enter" bind:value={links}/>
                 </div>
             </div>
         </AccordionItem>
 
-        <AccordionItem>
+        <AccordionItem open>
             {#snippet header()}
                 <span class="flex items-center">
                     <GraduationCapOutline size="md" class="me-1"/>
                     Education
                 </span>
             {/snippet}
-            <p>EDUCATION</p>
+            <Timeline>
+                {#each sortedEducation as school, i}
+                    {@const startDate = moment(school.startDate).format(DATE_FORMAT)}
+                    {@const endDate = school.ongoing ? "Present" : moment(school.endDate).format(DATE_FORMAT)}
+                    <TimelineItem title={school.school + school.id}
+                                  date={`${startDate} - ${endDate}`}
+                                  class="mb-4">
+                        <Button class="absolute top-0 right-0 p-0 bg-transparent! focus-within:ring-0!"
+                                onclick={() => {
+                                    education = education.filter(sch => sch.id !== school.id)
+                                }}>
+                            <CloseOutline size="sm"/>
+                        </Button>
+                        <p class="text-sm font-medium">{school.concentration}</p>
+                        <p class="text-sm font-light">{school.summary}</p>
+                    </TimelineItem>
+                {/each}
+            </Timeline>
+            <div class="grid grid-cols-2 gap-1.5">
+                <div class="col-span-full">
+                    <Label for="school" class="text-sm font-normal leading-6">School</Label>
+                    <Input size="md"
+                           name="school"
+                           bind:value={educationForm.school}
+                           placeholder="The University of Fictionville"/>
+                </div>
+                <div class="col-span-full">
+                    <Label for="concentration" class="text-sm font-normal leading-6">Concentration</Label>
+                    <Input size="md"
+                           name="concentration"
+                           bind:value={educationForm.concentration}
+                           placeholder="B.S. Electrical Engineering"/>
+                </div>
+                <div>
+                    <Label for="school-start" class="text-sm font-normal leading-6">Start Date</Label>
+                    <Datepicker id="school-start"
+                                dateFormat={{month: "short", year: "numeric"}}
+                                bind:value={educationForm.startDate}/>
+                </div>
+                <div>
+                    <Label for="school-end" class="text-sm font-normal leading-6">End Date</Label>
+                    <Datepicker id="school-end"
+                                dateFormat={{month: "short", year: "numeric"}}
+                                bind:value={educationForm.endDate}
+                                disabled={educationForm.ongoing}/>
+                    <Label class="float-right mt-1">
+                        Ongoing
+                        <Checkbox class="ml-0.5" bind:checked={educationForm.ongoing}/>
+                    </Label>
+                </div>
+                <div class="col-span-full">
+                    <Label for="school-summary" class="text-sm font-normal leading-6">Summary</Label>
+                    <Textarea name="school-summary"
+                              rows={3}
+                              bind:value={educationForm.summary}
+                              class="w-full bg-gray-700! border-gray-600!"
+                              placeholder="Add additional info or bulletpoints"/>
+                </div>
+                <Button class="col-span-full" onclick={() => {
+                    educationForm.id = education.length;
+                    education.push(educationForm);
+                    educationForm = {
+                        id: 0,
+                        school: "",
+                        concentration: "",
+                        startDate: undefined,
+                        endDate: undefined,
+                        ongoing: false,
+                        summary: ""
+                    };
+                }}>
+                    <span class="flex items-center">
+                        <PlusOutline size="sm" class="me-0.5"/>
+                        Add
+                    </span>
+                </Button>
+            </div>
         </AccordionItem>
 
         <AccordionItem>
